1b5e8282bf4138e129c4b08f7ccc987dc9aae196
Marcos <mcangar7@gmail.com>
2026-01-22 12:02:35 +0100

    refactor(src): Changed functions organization in gestion.c and interfaz.c

--- Practica6/include/gestion.h
+++ Practica6/include/gestion.h
@@ -5,14 +5,17 @@
 #include <stdint.h>
 #include "puntofijo.h"
 
+/**** Public functions declarations ****/
+extern void on_test_esp_moles_clicked(GtkWidget *widget, gpointer data);
+extern void on_test_auto_moles_clicked(GtkWidget *widget, gpointer data);
+extern void on_gen_moles_clicked(GtkWidget *widget, gpointer data);
 
-void on_test_esp_moles_clicked(GtkWidget *widget, gpointer data);
-void on_moles_value_change(GtkWidget *widget, gpointer data);
+extern void on_test_esp_densidad_clicked(GtkWidget *widget, gpointer data);
+extern void on_test_auto_densidad_clicked(GtkWidget *widget, gpointer data);
+extern void on_gen_densidad_clicked(GtkWidget *widget, gpointer data);
 
-void on_test_esp_densidad_clicked(GtkWidget *widget, gpointer data);
-void on_densidad_value_change(GtkWidget *widget, gpointer data);
-
-void on_test_esp_energia_clicked(GtkWidget *widget, gpointer data);
-void on_energia_value_change(GtkWidget *widget, gpointer data);
+extern void on_test_esp_energia_clicked(GtkWidget *widget, gpointer data);
+extern void on_test_auto_energia_clicked(GtkWidget *widget, gpointer data);
+extern void on_gen_energia_clicked(GtkWidget *widget, gpointer data);
 
 #endif
